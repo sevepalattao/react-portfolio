8bff87daf21b4797935dec5a9ea2cf9469f6a0a5
BigSev15 <palat008@umn.edu>
2023-02-02 12:06:54 -0600

    made header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import Navigation from './Navigation';
+
+export default function Header(props) {
+    const { current, setCurrent } = props
+
+    return (
+        <header>
+            <h1>
+                Seve Palattao
+            </h1>
+            <Navigation current={current} setCurrent={setCurrent}>
+            </Navigation>
+        </header>
+    );
+}
