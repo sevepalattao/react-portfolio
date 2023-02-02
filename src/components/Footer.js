86dabab0b42194449a0ae706cc92b9d90da21809
BigSev15 <palat008@umn.edu>
2023-02-02 15:32:36 -0600

    added footer component and starting app.js

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BsLinkedin from 'react-icons/bs';
+import BsGithub from 'react-icons/bs';
+
+export default function Footer() {
+    return(
+        <footer>
+            <ul>
+                <li>
+                    <a href='https://github.com/sevepalattao'>
+                        <BsGithub/>
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/seve-palattao-739956196/'>
+                        <BsLinkedin/>
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
