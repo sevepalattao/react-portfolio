02bf4b4fc5c056401b83ef3bdfc651bf6a4e9367
BigSev15 <palat008@umn.edu>
2023-02-09 15:08:39 -0600

    added project screenshots and starting on project component

---
+++ src/components/Project.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import JATE from '../assets/jate.png';
+import techBlog from '../assets/tech-blog.png';
+import foodTracker from '../assets/food-tracker.png';
+import marvelite from '../assets/marvelite.png';
+import weather from '../assets/weather.png';
+import codeQuiz from '../assets/code-quiz.png';
+
+const Project = () => {
+    const [projects] = useState([
+        {
+            name: "JATE"
+        }
+    ])
+}
