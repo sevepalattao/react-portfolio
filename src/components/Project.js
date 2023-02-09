3fed10edd31cf265ec159af73a8f967498daa6e7
BigSev15 <palat008@umn.edu>
2023-02-09 15:21:05 -0600

    added my projects to array in project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,46 @@ import codeQuiz from '../assets/code-quiz.png';
 const Project = () => {
     const [projects] = useState([
         {
-            name: "JATE"
+            name: "Just Another Text Editor",
+            image: JATE,
+            repo: 'https://github.com/sevepalattao/PWATextEditor',
+            deployed: 'https://pwa-texteditor-sevep.herokuapp.com/',
+        },
+        {
+            name: "The Tech Blog",
+            image: techBlog,
+            repo: 'https://github.com/sevepalattao/techBlogMVC',
+            deployed: 'https://tech-blog-mvc-sevep.herokuapp.com/',
+        },
+        {
+            name: "Food Tracker",
+            image: foodTracker,
+            repo: 'https://github.com/kipschetnan/Food-Tracker',
+            deployed: 'https://serene-taiga-18669.herokuapp.com/',
+        },
+        {
+            name: "Marvelite Hub",
+            image: marvelite,
+            repo: 'https://github.com/kipschetnan/Marvelite-Hub',
+            deployed: 'https://kipschetnan.github.io/Marvelite-Hub/',
+        },
+        {
+            name: "Weather App",
+            image: weather,
+            repo: 'https://github.com/sevepalattao/weatherApp',
+            deployed: 'https://sevepalattao.github.io/weatherApp/',
+        },
+        {
+            name: "Code Quiz",
+            image: codeQuiz,
+            repo: 'https://github.com/sevepalattao/web-APIs-codequiz',
+            deployed: 'https://sevepalattao.github.io/web-APIs-codequiz/',
         }
-    ])
+    ]);
+
+    return (
+        <div>
+            <h2>Portfolio</h2>
+        </div>
+    )
 }
